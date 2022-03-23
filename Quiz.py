90830f8e92b6b621eb4f718737a62951176a8d0a
bb2100 <albin.ekelunden@gmail.com>
2022-03-23 15:09:06 +0100

    Testade och fixade systemet med att fråga frågor

--- Quiz.py
+++ Quiz.py
@@ -8,9 +8,11 @@ class Question:
 
     def check_answer(self, answer):
         if answer == self.trueanswer :
+            print("Correct! \n")
             return 1
+            
         else:
-            print(f"That was incorrect. The correct answer was {self.trueanswer}")
+            print(f"That was incorrect. The correct answer was {self.trueanswer} \n")
         
 
     def write_alternatives(self):
@@ -23,24 +25,30 @@ class Quiz:
         self.score = score
 
     def ask_question(self):
-       ask = shuffle(self.question)
-       for x in self.question:
-           print (x.question)
-           q=0
-           for y in x.alternative:
+        ask = shuffle(self.question)
+        for x in self.question:
+            print (x.question)
+            q=0
+            for y in x.alternative:
                q+=1
-               print(f"{q}: {x}")
+               print(f"{q}: {y}")
             answer = input("Write the letter of the alternative:")
-            if y.check_answer(answer) == 1 #Needs controlling answer
-
+            if x.check_answer(answer) == 1: #Needs controlling answer
+                self.score += 1
 
 
     def question_number(self):
         pass
 
-q1 = Question("Hur många bekräftade testiklar har Frej Larsson","c",["a) 1st","b) 2st","c) 3st","d) Inga, bara en manpussy"])
 
-quiz = Quiz("bbsQuiz",[q1],0)
+def main():
+    q1 = Question("Hur många bekräftade testiklar har Frej Larsson","c",["a) 1st","b) 2st","c) 3st","d) Inga, bara en manpussy"])
+    q2 = Question("Hur bra är Pussy Lover av DJ Balloon", "a",["a) 11/10","b) 2/10","c) 0/10","d) 6/10"])
+    q3 = Question("Vad tycker bb2100 om Niclas", "d",["a) Asjobbig", "b) Fett king", "c) För nördig", "d) Daddy <3"])
+    q4 = Question("Hur uppkom namnet bb2100?","a",["a) Tyckte det lät som robot ca 2013","b) Baserat på starwars bb8","c) Bytte från dd2100","d) Slog nåt random på tangentbordet"])
 
-
+    quiz = Quiz("bbsQuiz",[q1,q2,q3,q4],0)
+    quiz.ask_question()
+if __name__ == "__main__":
+    main()
 
