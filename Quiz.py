0c7b76f9860ec335079180d8e20ad2b81706ee67
albin <albin.ekelunden@gmail.com>
2022-03-28 21:35:34 +0200

    Gjorde färdigt Quiz2

--- Quiz.py
+++ Quiz.py
@@ -60,9 +60,18 @@ def main(): #Questions and creating quiz
     
     #quiz 2
     q2_0 = Question("Har delfiner hår?","c",["a) Ja", "b) nej", "c) Bara när dem föds", "d) Bara precis innan dem dör"])
+    q2_1 = Question("På vilken StarWars planet kan man höra frasen ''its over Anakin, i have the highground''?","a",["a) Mustafar","b) Coruscant","c) Alderan","d) Naboo"])
+    q2_2 = Question("Vilket år grundades spotify?","b",["a) 2002","b) 2006","c) 1998","d) 2010"])
+    q2_3 = Question("Vilket år kom albumet Framgång och Efterfrågan av maskinen ut?","b",["a) 1996","b) 2012","c) 2009","d) 2015"])
+    q2_4 = Question("När började man använda konservburkar?","d",["a) 1776","b) 1684","c) 1910","d) 1810"])
+    q2_5 = Question("När skapades steam?","b",["a) 2021","b) 2003","c) 2001","d) 2010"])
+    q2_6 = Question("Var skapades Coca Cola","a",["a) Georgia","b) Oregon","c) Danmark","d) California"])
+    q2_7 = Question("Hur många människor hade någonsin levt 2020?","c",["a) 7,772,850,162","b) 200,324,561,413","c) 116,761,402,413","d) 109"])
+    q2_8 = Question("Vad är det aktiva ämnet i marijuana?","c",["a) MDMA","b) CO2","c) THC","d) PCP"])
+    q2_9 = Question("Vad betyder slangordet sauce?","b",["a) sås","b) Källa för pornografiskt innehåll","c) Sprit","d) En väldigt överviktig indevid"])
 
     quiz = Quiz("bbsQuiz",[q1_0,q1_1,q1_2,q1_3,q1_4,q1_5,q1_6,q1_7,q1_8,q1_9],0,1)
-    quiz2 = Quiz("FaktaQuiz",[q2_0],0,1)
+    quiz2 = Quiz("FaktaQuiz",[q2_0,q2_1,q2_2,q2_3,q2_4,q2_5,q2_6,q2_7,q2_8,q2_9],0,1)
 
     #Below is the thing that chooses quiz and calls for the question
     start = input("Vilket quiz vill du starta? \nSkriv 1 för bbsQuiz\nSkriv 2 för FaktaQuiz\nSkriv nåt annat för att avsluta\n")
